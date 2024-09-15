ae4e4ae405a2ce7c4976bb98ffbde87f522dcb95
Yusuf Quadri Kolawole <quadyusuf@gmail.com>
2024-09-15 17:09:23 +0100

    end of exploration

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import {
+  bootstrapApplication,
+  provideProtractorTestingSupport,
+} from "@angular/platform-browser";
+import { provideRouter } from "@angular/router";
+import routeConfig from "./app/routes";
+import { AppComponent } from "./app/app.component";
+
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
+}).catch((err) => console.error(err));
